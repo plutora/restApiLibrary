0585f7aecc95b6412863655dd04d29869d07715f
Greg Maxey <greg.maxey@plutora.com>
2017-05-05 11:29:32 -0400

    Utilize getComponentVersion()

--- JenkinsDemoUpdateVersion.py
+++ JenkinsDemoUpdateVersion.py
@@ -5,16 +5,24 @@
 import plutora
 from optparse import OptionParser
 
-def updateVersion(version,environment):
-	envGuid = plutora.guidByPathAndName("environments", environment, field="name")
-	envData = plutora.api("GET","environments/"+envGuid)
-	layerGuid = envData['hosts'][0]['layers'][0]['id']
-	layerData = plutora.api("GET","layers/"+layerGuid)
+def updateVersion(version,path):
+	componentGuid = plutora.getComponentVersion(path)
+	layerData = plutora.api("GET","layers/"+componentGuid)
 	layerData['version']=version
-	plutora.api("PUT","layers/"+layerGuid,layerData)
-	
+	plutora.api("PUT","layers/"+componentGuid,layerData)
+
+# Get command line arguments
 parser = OptionParser()
 (options, args) = parser.parse_args()
 version = args[0]
 phase = args[1]
-updateVersion(version,"Jenkins - " + phase)
+
+# Create path to the component to be updated
+path = {
+	"environmentName": "Jenkins - " + phase,
+	"hostName": "jenkinsDemoDev",
+	"layerType": "Application",
+	"componentName": "JenkinsWebDemo"
+}
+
+updateVersion(version,path)
