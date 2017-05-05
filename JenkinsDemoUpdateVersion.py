acd9d02c15144dc3bed6888ddeb9ff2bfa73c159
Greg Maxey <greg.maxey@plutora.com>
2017-05-05 10:45:56 -0400

    Refactor and doc

--- JenkinsDemoUpdateVersion.py
+++ JenkinsDemoUpdateVersion.py
@@ -1,10 +1,14 @@
+# Python 2.7
+#
+# USAGE: python JenkinsDemoUpdateVersion.py <version value> <phase: DEV|QA|UAT>
+#
 import plutora
 from optparse import OptionParser
 
-def updateVersion(version,phase):
-	devGuid = plutora.guidByPathAndName("environments", "Jenkins demo - " + phase, field="name")
-	devData = plutora.api("GET","environments/"+devGuid)
-	layerGuid = devData['hosts'][0]['layers'][0]['id']
+def updateVersion(version,environment):
+	envGuid = plutora.guidByPathAndName("environments", environment, field="name")
+	envData = plutora.api("GET","environments/"+envGuid)
+	layerGuid = envData['hosts'][0]['layers'][0]['id']
 	layerData = plutora.api("GET","layers/"+layerGuid)
 	layerData['version']=version
 	plutora.api("PUT","layers/"+layerGuid,layerData)
@@ -13,4 +17,4 @@ parser = OptionParser()
 (options, args) = parser.parse_args()
 version = args[0]
 phase = args[1]
-updateVersion(version,phase)
+updateVersion(version,"Jenkins - " + phase)
