db73d8d49c3671595f777e4445e8ac6fe977e59b
Greg Maxey <greg.maxey@plutora.com>
2017-08-07 13:47:34 -0400

    delete items

--- checkHosts.py
+++
@@ -1,9 +0,0 @@
-import plutora
-
-f = open('deletedHosts.txt','r')
-
-for host in f:
-	hostGuid = host.strip()
-	hostdata = plutora.api("GET","hosts/"+hostGuid)
-	if hostdata=="":
-		print hostGuid
--- delete.py
+++
@@ -1,6 +0,0 @@
-import plutora
-
-environments = plutora.api("GET", "environments")
-for environment in environments:
-	if ("API" in environment['name']):
-		print plutora.api("DELETE", "environments/" + environment['id'])
